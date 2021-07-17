100fd1026a2db5a6b278e326b3b62adbdbc2ca7d
cristiano.silva <cristiano.silva@tecsinapse.com.br>
2021-07-17 16:13:43 -0300

    Change on the Parentheses rule, now each parentheses is represented by a case class

--- src/main/scala/dev/cgss/Main.scala
+++ src/main/scala/dev/cgss/Main.scala
@@ -1,7 +1,7 @@
 package dev.cgss
 
+import dev.cgss.token.lexical.Parentheses
+
 object Main extends App {
-
-
 
 }
--- src/main/scala/dev/cgss/token/lexical/Parentheses.scala
+++ src/main/scala/dev/cgss/token/lexical/Parentheses.scala
@@ -1,11 +1,30 @@
 package dev.cgss.token.lexical
 
-final case class Parentheses(char: Char) extends LexicalToken
+abstract class Parentheses(c: Char) extends LexicalToken {
+  override def char: Char = c
+}
+
+final case class OpenParentheses() extends Parentheses('(')
+
+final case class CloseParentheses() extends Parentheses(')')
+
+final case class OpenSquareBrackets() extends Parentheses('[')
+
+final case class CloseSquareBrackets() extends Parentheses(']')
+
+final case class OpenCurlyBrackets() extends Parentheses('{')
+
+final case class CloseCurlyBrackets() extends Parentheses('}')
 
 object Parentheses {
 
   def apply(char: Char): Parentheses = char match {
-    case '(' | ')' | '[' | ']' | '{' | '}' => new Parentheses(char)
+    case '(' => OpenParentheses()
+    case ')' => CloseParentheses()
+    case '[' => OpenSquareBrackets()
+    case ']' => CloseSquareBrackets()
+    case '{' => OpenCurlyBrackets()
+    case '}' => CloseCurlyBrackets()
     case _ => throw LexicalTokenException(s"Unknown parentheses $char!")
   }
 
