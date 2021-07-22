439b6aa6e993b164c081a5cf78f3352f195d51f6
cristiano.silva <cristiano.silva@tecsinapse.com.br>
2021-07-22 19:07:56 -0300

    Adding IdentifierToken that contains and parses the identifier set of rules

--- src/main/scala/dev/cgss/Main.scala
+++ src/main/scala/dev/cgss/Main.scala
@@ -4,6 +4,4 @@ import dev.cgss.token.lexical.enrichString
 
 object Main extends App {
 
-  println("adjksh45t4ekjfdsk".toLexicalTokens)
-
 }
---
+++ src/main/scala/dev/cgss/token/identifier/IdentifierToken.scala
@@ -0,0 +1,25 @@
+package dev.cgss.token.identifier
+
+import dev.cgss.token.lexical.LexicalToken
+
+/*
+op       ::=  opchar {opchar}
+varid    ::=  lower idrest
+boundvarid ::=  varid
+| ‘`’ varid ‘`’
+plainid  ::=  upper idrest
+|  varid
+|  op
+id       ::=  plainid
+|  ‘`’ { charNoBackQuoteOrNewline | escapeSeq } ‘`’
+idrest   ::=  {letter | digit} [‘_’ op]
+escapeSeq     ::= UnicodeEscape | charEscapeSeq
+UnicodeEscape ::= ‘\’ ‘u’ {‘u’} hexDigit hexDigit hexDigit hexDigit
+hexDigit      ::= ‘0’ | … | ‘9’ | ‘A’ | … | ‘F’ | ‘a’ | … | ‘f’
+*/
+
+trait IdentifierToken {
+
+  def apply(lexicalTokenSeq: Seq[LexicalToken]): IdentifierToken = ???
+
+}
